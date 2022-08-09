c8c37411a0f100f40167f1caac41f20390d142af
Myroslav Sokolov <mirossoks@gmail.com>
2022-08-09 22:09:07 +0300

    first page

--- react-native-app/App.js
+++ react-native-app/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import {
   StyleSheet,
@@ -5,20 +6,54 @@ import {
   View,
   ImageBackground,
   TextInput,
+  TouchableOpacity,
+  Platform,
+  KeyboardAvoidingView,
 } from "react-native";
 
 export default function App() {
+  const [isShowKeyboard, setIsShowKeyboard] = useState(false);
   return (
     <View style={styles.container}>
       <ImageBackground
         style={styles.image}
-        source={require("./src/images/background-6556393_1280.jpg")}
+        source={require("./src/images/pexels-lukas-669576.jpg")}
       >
-        <Text style={styles.title}>your aapp!</Text>
-        <TextInput style={styles.input} />
+        <KeyboardAvoidingView behavior={Platform.OS === "ios" && "padding"}>
+          <View
+            style={{
+              ...styles.form,
+              ...Platform.select({
+                ios: { marginBottom: isShowKeyboard ? 100 : 0},
+                android: {},
+              }),
+            }}
+          >
+            <View>
+              <Text style={styles.inputTitle}>EMAIL</Text>
+              <TextInput
+                style={styles.input}
+                onFocus={() => setIsShowKeyboard(true)}
+              />
+            </View>
+            <View style={{ marginTop: 20 }}>
+              <Text style={styles.inputTitle}>PASSWORD</Text>
+              <TextInput
+                style={styles.input}
+                secureTextEntry
+                onFocus={() => setIsShowKeyboard(true)}
+              />
+            </View>
+            <View>
+              <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+                <Text style={styles.buttonText}>SIGN IN</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </KeyboardAvoidingView>
       </ImageBackground>
 
-      {/* <StatusBar style="auto" /> */}
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -28,24 +63,43 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#000",
   },
-  title: {
-    fontSize: 30,
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    marginBottom: 10,
-  },
   image: {
     flex: 1,
     resizeMode: "cover",
     justifyContent: "center",
   },
+  form: {
+    marginHorizontal: 40,
+    // ...Platform.select({
+    //   ios: { marginBottom: 100 },
+    //   android: {},
+    // }),
+  },
+  inputTitle: {
+    fontSize: 30,
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginBottom: 10,
+  },
   input: {
+    textAlign: "center",
     borderColor: "#fff",
     borderWidth: 1,
-    marginHorizontal: 10,
-    paddingHorizontal: 5,
+    height: 40,
+    borderRadius: 6,
+    fontSize: 20,
     color: "#fff",
-    borderRadius: 10,
-    fontSize: 25,
+  },
+  button: {
+    height: 40,
+    borderRadius: 6,
+    marginTop: 120,
+    justifyContent: "center",
+    alignItems: "center",
+    marginHorizontal: 60,
+    backgroundColor: "#000",
+  },
+  buttonText: {
+    color: "#fff",
   },
 });
