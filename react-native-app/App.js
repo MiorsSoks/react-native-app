ed01693d447ed2ca0069cb3a88d0e96beb38c875
Myroslav Sokolov <mirossoks@gmail.com>
2022-08-10 16:16:58 +0300

    add hide keyboard

--- react-native-app/App.js
+++ react-native-app/App.js
@@ -9,49 +9,65 @@ import {
   TouchableOpacity,
   Platform,
   KeyboardAvoidingView,
+  Keyboard,
+  TouchableWithoutFeedback,
 } from "react-native";
 
 export default function App() {
   const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+  function keyboardHide() {
+    setIsShowKeyboard(false);
+    Keyboard.dismiss();
+  }
   return (
     <View style={styles.container}>
-      <ImageBackground
-        style={styles.image}
-        source={require("./src/images/pexels-lukas-669576.jpg")}
-      >
-        <KeyboardAvoidingView behavior={Platform.OS === "ios" && "padding"}>
-          <View
-            style={{
-              ...styles.form,
-              ...Platform.select({
-                ios: { marginBottom: isShowKeyboard ? 100 : 0},
-                android: {},
-              }),
-            }}
-          >
-            <View>
-              <Text style={styles.inputTitle}>EMAIL</Text>
-              <TextInput
-                style={styles.input}
-                onFocus={() => setIsShowKeyboard(true)}
-              />
-            </View>
-            <View style={{ marginTop: 20 }}>
-              <Text style={styles.inputTitle}>PASSWORD</Text>
-              <TextInput
-                style={styles.input}
-                secureTextEntry
-                onFocus={() => setIsShowKeyboard(true)}
-              />
-            </View>
-            <View>
-              <TouchableOpacity style={styles.button} activeOpacity={0.7}>
-                <Text style={styles.buttonText}>SIGN IN</Text>
-              </TouchableOpacity>
-            </View>
+      <TouchableWithoutFeedback onPress={()=> keyboardHide()}>
+        <ImageBackground
+          style={styles.image}
+          source={require("./src/images/pexels-lukas-669576.jpg")}
+        >
+          <View style={styles.header}>
+            <Text style={styles.headerText}>Welcome to "My progress"!</Text>
+            <Text style={styles.headerText}>Please sign in or register</Text>
           </View>
-        </KeyboardAvoidingView>
-      </ImageBackground>
+          <KeyboardAvoidingView behavior={Platform.OS === "ios" && "padding"}>
+            <View
+              style={{
+                ...styles.form,
+                ...Platform.select({
+                  ios: { marginBottom: isShowKeyboard ? 100 : 0 },
+                  android: { marginBottom: isShowKeyboard ? 100 : 0 },
+                }),
+              }}
+            >
+              <View>
+                <Text style={styles.inputTitle}>EMAIL</Text>
+                <TextInput
+                  style={styles.input}
+                  onFocus={() => setIsShowKeyboard(true)}
+                />
+              </View>
+              <View style={{ marginTop: 20 }}>
+                <Text style={styles.inputTitle}>PASSWORD</Text>
+                <TextInput
+                  style={styles.input}
+                  secureTextEntry
+                  onFocus={() => setIsShowKeyboard(true)}
+                />
+              </View>
+              <View>
+                <TouchableOpacity
+                  style={styles.button}
+                  activeOpacity={0.7}
+                  onPress={() => keyboardHide()}
+                >
+                  <Text style={styles.buttonText}>SIGN IN</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </KeyboardAvoidingView>
+        </ImageBackground>
+      </TouchableWithoutFeedback>
 
       <StatusBar style="auto" />
     </View>
@@ -68,12 +84,16 @@ const styles = StyleSheet.create({
     resizeMode: "cover",
     justifyContent: "center",
   },
+  header: {
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginBottom: 100,
+  },
+  headerText: {
+    fontSize: 30,
+  },
   form: {
     marginHorizontal: 40,
-    // ...Platform.select({
-    //   ios: { marginBottom: 100 },
-    //   android: {},
-    // }),
   },
   inputTitle: {
     fontSize: 30,
@@ -93,7 +113,7 @@ const styles = StyleSheet.create({
   button: {
     height: 40,
     borderRadius: 6,
-    marginTop: 120,
+    marginTop: 40,
     justifyContent: "center",
     alignItems: "center",
     marginHorizontal: 60,
